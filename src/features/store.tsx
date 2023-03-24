f44f4844e7cc990d65daae9fe7797f655723988c
Clara Daudre-Vignier <cladaudr@WKMZTCE806F8.global.publicisgroupe.net>
2023-03-24 18:13:22 +0000

    Clean branch

--- src/features/store.tsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import featureReducer from './feature/featureSlice'
-
-export const store = configureStore({
-  reducer: {
-    feature: featureReducer
-  }
-})
--- src/pages/Page2.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Page2 = () => {
-  return <h1>pg2</h1>
-}
-
-export default Page2
