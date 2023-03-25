ee74e678d997f36571648b02c332a310f7e7d044
William Ashbury <william.ashbury@publicissapient.com>
2023-03-25 09:15:37 +0000

    reformatted file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "@fontsource/raleway/400.css";
 import React from 'react';
-import { NavBar } from "./NavBar";
+import { NavBar } from "./components/NavBar";
 import { Footer } from "./components/Footer";
-import { Main_content } from "./Main_content";
+import { Main_content } from "./pages/Main_content";
 import {
   ChakraProvider,
   Box,
