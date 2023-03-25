38608ef5c605cb244b0ce169de393ff34c337137
William Ashbury <william.ashbury@publicissapient.com>
2023-03-25 09:34:03 +0000

    added routing and redux framework

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
+import { store } from "./features/store";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <ChakraProvider>
-    <App />
-  </ChakraProvider>
-  , rootElement );
-
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ChakraProvider>,
+  rootElement
+);
